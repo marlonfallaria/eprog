9d68bcc08c31724965baebb7d59058986b8653c2
Marlon G. Fallaria <marlonfallaria91@gmail.com>
2016-12-13 08:58:09 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+	int a, b, c, d;
+	
+	printf("Enter value for a: ");
+	scanf("%d", &a);
+	printf("Enter value for b: ");
+	scanf("%d", &b);
+	printf("Enter value for c: ");
+	scanf("%d", &c);
+	printf("%d", d = (a+b+c)/3);
+}
