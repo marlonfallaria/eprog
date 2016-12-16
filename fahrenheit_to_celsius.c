d370deb2da05f86108cf9073680b6161654c2dc5
Marlon G. Fallaria <marlonfallaria91@gmail.com>
2016-12-17 00:40:57 +0800

    Create fahrenheit_to_celsius.c

---
+++ fahrenheit_to_celsius.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+	int F, C;
+  
+	printf ("Temp. in Farenheit : ");
+	scanf ("%d", &F);
+	C = (F-32);
+	printf ("Temp. in Celsius : ");
+	printf ("%d", 	C = (F-32)*5/9);
+  
+}
