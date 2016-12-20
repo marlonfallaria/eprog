1b37cfba0807bdcba197bbbc92268f673f1fce84
Marlon G. Fallaria <marlonfallaria91@gmail.com>
2016-12-20 08:15:06 +0800

    Update if_play.c

--- if_play.c
+++ if_play.c
@@ -3,13 +3,13 @@ int main(){
 	int num;
 	
 	printf("Enter number : ");
-		scanf("%d", &num);
-		printf("Number is : %d", num);	
-		if(num % 3 == 0){
-			printf(" Ed");
-		}
-		if(num % 5 == 0){
-			printf(" Pogi");
-		}
+	scanf("%d", &num);
+	printf("Number is : %d", num);	
+	if(num % 3 == 0){
+		printf(" Ed");
+	}
+	if(num % 5 == 0){
+		printf(" Pogi");
+	}
 		
 }
