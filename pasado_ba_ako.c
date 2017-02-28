cc135edba246f5ed5f41c784f16dceeed4e2f245
Marlon G. Fallaria <marlonfallaria91@gmail.com>
2017-02-28 09:38:03 +0800

    Update pasado_ba_ako.c

--- pasado_ba_ako.c
+++ pasado_ba_ako.c
@@ -1,108 +1,148 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
-struct detail{
-	long id;
-	float gpa;
-};
 
-struct student{  
+struct detail{
+	char course[30];
+	float grade;
+};
+	
+struct student{
 	char name[30];
 	struct detail info;
-}; 
+};
 
-void DisplayMenu(){
+
+
+void displayMenu(){
 	printf("0 - Exit\n");
-	printf("1 - Write to File\n");
+	printf("1 - Write to file\n");
 	printf("2 - Read file\n");
-	printf("3 - Display Passed\n");
-	printf("4 - Display Failed\n");
-	printf("5 - Display Invalid\n");
+	printf("3 - Passed\n");
+	printf("4 - Failed\n");
+	printf("5 - Invalid Grade\n");
 }
 
-void WriteToFile(){
+void writeToFile(){
 	FILE *myFile;
 	struct student stud;
 	myFile = fopen("sample.txt", "a");
-	printf("Enter student name: ");
-	scanf("%s", &stud.name);
-	printf("Enter Program/Course: ");
-	scanf("%s", &stud.info.id);
-	printf("Enter student Grade: ");
-	scanf("%f", &stud.info.gpa);	
+	
+	printf("Enter Student Name: ");
+		scanf("%s", &stud.name);
+	printf("Enter Course: ");
+		scanf("%s", &stud.info.course);
+	printf("Enter Student Grade: ");
+		scanf("%f", &stud.info.grade);
+	printf("%s %s %.2f\n", stud.name, stud.info.course, stud.info.grade);
+		fprintf(myFile, "%s %s %.2f\n", stud.name, stud.info.course, stud.info.grade);
 	fclose(myFile);
 }
 
-void PassedStud(){
-	FILE *myFile;
-	struct student stud;
-	myFile = fopen("sample.txt", "a");
-		printf("Students who have passed:\n");
-	if(stud.info.gpa >= 75){
-		printf("%s \n", stud.name);
-	}	
-	fclose(myFile);
-}
-
-void FailedStud(){
-		FILE *myFile;
-	struct student stud;
-	myFile = fopen("sample.txt", "a");
-		printf("Students who have failed:\n");
-	if(stud.info.gpa < 75){
-		printf("%s \n", stud.name);
-	}	
-	fclose(myFile);
-}
-void ReadFromFile(){
+void readFromFile(){
 	struct student stud;
 	FILE *myFile;
 	myFile = fopen("sample.txt", "r");
-    if(myFile == NULL){
-    	printf("Error: File does not exist");
+
+	if(myFile == NULL){
+		printf("Error: File does not exist");
+	exit(1);
 	}
+
 	char temp[128];
 	while (fgets(temp, sizeof temp, myFile) != NULL) {
-		sscanf(temp, "%s %d %f", &stud.name, &stud.info.id, &stud.info.gpa);
-		printf("%s %d %f\n", stud.name, stud.info.id, stud.info.gpa);
+		sscanf(temp, "%s %s %f", &stud.name, &stud.info.course, &stud.info.grade);
+		printf("%s %s %f\n", stud.name, stud.info.course, stud.info.grade);
+	}
+	fclose(myFile);
+	}
+
+void displayPassed(){
+	struct student stud;
+	FILE *myFile;
+	myFile = fopen("sample.txt", "r");
+
+	if(myFile == NULL){
+		printf("Error: File does not exist");
+		exit(1);
+	}	
+
+char temp[128];
+	while (fgets(temp, sizeof temp, myFile) != NULL) {
+		sscanf(temp, "%s %s %f", &stud.name, &stud.info.course, &stud.info.grade);
+		if((stud.info.grade>=75)&&(stud.info.grade<=100)){
+			printf("%s %s %f\n", stud.name, stud.info.course, stud.info.grade);
+		}
+	}
+	fclose(myFile);
+	}
+
+void displayFailed(){
+	struct student stud;
+	FILE *myFile;
+	myFile = fopen("sample.txt", "r");
+
+	if(myFile == NULL){
+		printf("Error: File does not exist");
+		exit(1);
+	}
+
+	char temp[128];
+	while (fgets(temp, sizeof temp, myFile) != NULL) {
+		sscanf(temp, "%s %s %f", &stud.name, &stud.info.course, &stud.info.grade);
+		if((stud.info.grade>0)&&(stud.info.grade<75)){
+			printf("%s %s %f\n", stud.name, stud.info.course, stud.info.grade);
+		}	
 	}
 	fclose(myFile);
 }
 
-void InvalidGrade(){
-		struct student stud;
+void displayInvalid(){
+	struct student stud;
 	FILE *myFile;
-	myFile = fopen("sample.txt", "a");
-	printf("Students with invalid grade:\n");
-	if(stud.info.gpa < 0){
-		printf("%s \n", stud.name);
+	myFile = fopen("sample.txt", "r");
+
+	if(myFile == NULL){
+		printf("Error: File does not exist");
+	exit(1);
 	}
+
+	char temp[128];
+	while (fgets(temp, sizeof temp, myFile) != NULL) {
+		sscanf(temp, "%s %s %f", &stud.name, &stud.info.course, &stud.info.grade);
+		if((stud.info.grade<0)||(stud.info.grade>100)){
+		printf("%s %s %f\n", stud.name, stud.info.course, stud.info.grade);
+		}
+	}
+	fclose(myFile);
 }
 
-int main (){	
-    int choice = 1;
-    DisplayMenu();
+int main (){
+int choice = 1;
+
+displayMenu();
+scanf("%d", &choice);
+while(choice != 0){
+    switch(choice){
+        case 0: 
+            exit(1);
+            break;
+        case 1: 
+            writeToFile();
+            break;
+        case 2: 
+            readFromFile();
+            break;
+        case 3:
+            displayPassed();
+            break;
+        case 4:
+            displayFailed();
+            break;
+        case 5:
+            displayInvalid();
+            break;
+    }
+    displayMenu();
     scanf("%d", &choice);
-    while(choice != 0){
-    	switch(choice){
-    		case 0: 
-    			break;
-    		case 1: 
-    			WriteToFile();
-    			break;
-    		case 2: 
-    			ReadFromFile();
-    			break;
-    		case 3:
-    			PassedStud();
-    			break;
-    		case 4:
-    			FailedStud();
-    			break;
-    		case 5:
-    			InvalidGrade();
-    			break;
-		}
-		DisplayMenu();
-    	scanf("%d", &choice);
-	}    
+}    
 }
